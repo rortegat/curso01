732b619c25827dd6fccbb971c2c6381af1e4fb2a
Ricardo Ortega <ricardo.ortega.tapia@hotmail.com>
2020-08-10 16:03:35 -0500

    initial commit

---
+++ hola_mundo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+//Inicio
+    printf( "Hola mundo" );
+//fin	
+    return 0;
+}
---
+++ suma.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int x;
+    int y;
+    int z;
+
+    printf("Dame x: ");
+    scanf("%i",&x);
+
+    printf("Dame y: ");
+    scanf("%i",&y);
+
+    z = x + y;
+
+    printf("%i", z);
+
+    return 0;
+}
